2d797ce5f7f7094a9950ee9e745732f197f0b3c6
Lori Culberson <bigskybandit@sbcglobal.net>
2022-02-19 13:21:12 -0500

    Add ant card

--- src/components/sports/Sports.js
+++ src/components/sports/Sports.js
@@ -1,5 +1,28 @@
+import './sport.css'
+import 'antd/dist/antd.css';
+import sportsData from '../../data/sportsData'
+import { Card } from 'antd'
+const { Meta } = Card
+
+console.log(sportsData)
 export default function Sports(){
+  const cardList = sportsData.map(sport => {
+    return (
+      <Card
+        key={sport.id}
+        hoverable
+        style={{ width: 240 }}
+        cover={<img className="sport-img" alt={sport.title} src={sport.imageSrc} />}
+      >
+      <Meta title={sport.title} description="www.instagram.com" />
+      </Card>
+    )
+  })
+
   return(
-    <div className="sports"><h1>Welcome to the Sports Section</h1></div>
+    <div>
+      <h1>Welcome to the Sports Section</h1>
+      {cardList}
+    </div>
   )
 }
